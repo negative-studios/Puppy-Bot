88609cfa0f0674f85dd7bc985792a585526cd5fa
Austin <HunterKillerDrone154@gmail.com>
2019-03-16 00:45:54 -0400

    Create developerinfo.js

---
+++ commands/developerinfo.js
@@ -0,0 +1,10 @@
+module.exports.run = (bot, message, args, Discord) => {
+let info = new Discord.RichEmbed()
+.setTitle("Developer Information")
+.addField("Developer:", "<@513103852409716736>")
+.addField("Studio:", "Join [here](https://discord.gg/XV27psT)")
+}
+
+module.exports.help = {
+"name": "developerinfo"
+}
