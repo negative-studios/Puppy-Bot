6155d704ef074e17bb4283d585f2e036d95f1b72
Austin <HunterKillerDrone154@gmail.com>
2019-03-16 00:47:05 -0400

    Update developerinfo.js

--- commands/developerinfo.js
+++ commands/developerinfo.js
@@ -3,6 +3,7 @@ let info = new Discord.RichEmbed()
 .setTitle("Developer Information")
 .addField("Developer:", "<@513103852409716736>")
 .addField("Studio:", "Join [here](https://discord.gg/XV27psT)")
+message.channel.send({embed: info})
 }
 
 module.exports.help = {
