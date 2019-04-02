fa2fb0be0f3a6d50b064142869c7240ce29cb2d5
Austin <HunterKillerDrone154@gmail.com>
2019-04-02 00:45:48 -0400

    Create rng.js

---
+++ commands/rng.js
@@ -0,0 +1,32 @@
+module.exports.run = (bot, message, args, Discord) => {
+let numbermax = args.join(" ")
+//Set's the X in 0-X
+let num = Math.floor(Math.random()*numbermax);
+let em = new Discord.RichEmbed()
+.setTitle("Random Number Generator")
+.setDescription("I guess I'm here to make a number for you. M'lord")
+.addField("Your number is:" num)
+.setColor("GREEN")
+
+let emf = new Discord.RichEmbed()
+.setTitle("Oopsie Woopsi!")
+.setDescription("Your ment to put a number for the args silly!")
+.setColor("RED")
+
+let emf2 = new Dicord.RichEmbed()
+.setTitle("Oppsi Woopsi V2!")
+.setDescription("Please include a number after !rng!!")
+.setColor("RED")
+
+if(!numbermax) {
+message.channel.send({embed: emf2})
+} else if(num == "NaN") {
+message.channel.send({embed: emf})
+} else {
+message.channel.send({embed: em})
+}
+}
+
+module.exports.help = {
+"name": "rng"
+}
