8ce93e292e49cf944f826bce91b3ab4a05d16660
Austin <HunterKillerDrone154@gmail.com>
2019-04-02 00:51:17 -0400

    ergg
    I want to die

--- commands/rng.js
+++ commands/rng.js
@@ -18,12 +18,9 @@ let emf2 = new Discord.RichEmbed()
 .setDescription("Please include a number after !rng!!")
 .setColor("RED")
 
-if(!numbermax) {
-message.channel.send({embed: emf2})
-} else if(num == "NaN") {
-message.channel.send({embed: emf})
-} else {
-message.channel.send({embed: em})
+if(!numbermax) message.channel.send({embed: emf2})
+if(num === "NaN") message.channel.send({embed: emf})
+if(!numbermax || !num === "NaN") message.channel.send({embed: em})
 }
 }
 
